0009cac20bcdd372dba4531750f17ae7fd8ea6ac
Ahhhhhyeong <dkdud081@gmail.com>
2025-06-24 16:01:15 +0900

    #8 중고거래 물품 데이터 나열

--- carrot/src/pages/ProductList.jsx
+++ carrot/src/pages/ProductList.jsx
@@ -5,24 +5,30 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 //Components
 import ProductAside from '../components/product-list/ProductAside';
 import ProductItemList from '../components/product-list/ProductItemList';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function ProductList() {
   return (
-    <div className='market__list'>
-      <nav className='market__nav__info'>
-        <ol>
-          <li>홈</li>
-          <li>{'>'}</li>
-          <li>중고거래</li>
-        </ol>
-      </nav>
-      <h2 className='market__now__location'>
-        서울특별시 서초구 서초동 중고거래
-      </h2>
-      <section className='market__item__section'>
-        <ProductAside />
-        <ProductItemList />
-      </section>
+    <div className='market__list-container'>
+      <div className='market__list'>
+        <nav className='market__nav__info'>
+          <ol>
+            <li>
+              <Link to={'/'}>홈</Link>
+            </li>
+            <li>{'>'}</li>
+            <li>중고거래</li>
+          </ol>
+        </nav>
+        <h2 className='market__now__location'>
+          {/* 서울특별시 서초구 {localData} 중고거래 */}
+          서울특별시 서초구 서초동 중고거래
+        </h2>
+        <section className='market__item__section'>
+          <ProductAside />
+          <ProductItemList />
+        </section>
+      </div>
     </div>
   );
 }
