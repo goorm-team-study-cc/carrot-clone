2e3ba644115ea2f790e77cf532e2f538c9ba8b23
박혜경 <parkhyegyeong@baghyegyeong-ui-MacBookAir.local>
2025-06-25 14:19:06 +0900

    처음 커밋

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './App.css'
+import Head from './Carrot/Head'
+import Body from './Carrot/body'
+import Foot from './Carrot/Foot'
+
+function App() {
+  
+
+  return (
+    <>
+      <Body/>
+      <Foot/>
+      <Head/>
+
+    </>
+  )
+}
+
+export default App
