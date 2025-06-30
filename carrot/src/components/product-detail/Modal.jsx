c6f51e4402af1e5da35fb7f58bd134c6c2ccc0e4
6108 <>
2025-06-30 16:49:44 +0900

    #1 모달 수정

--- carrot/src/components/product-detail/Modal.jsx
+++ carrot/src/components/product-detail/Modal.jsx
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
+import qr from '../../assets/qr.png';
 
 export default function Modal({ dispatch }) {
 
   return (
     <>
-      <div className="modal">
-        <div className="modal-bg"></div>
-        <div className="modal-box">
-          <button onClick={dispatch}>close</button>
-          <h1>큐알코드</h1>
+      <div className="modal-bg"></div>
+      <div className="modal-box">
+        <div className="flex-row">
+          <span>QR 코드 스캔</span>
+          <span className="ml-auto" onClick={dispatch}>X</span>
         </div>
+        <div>
+          <div className="subtitle margin-tb">당근 앱으로 열기</div>
+          <img src={qr} alt="" />
+        </div>
+
       </div>
+
     </>
   );
 }
