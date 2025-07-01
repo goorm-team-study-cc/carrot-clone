90703b0defa218fd4dc57b233238bde395ce7d94
6108 <>
2025-07-01 16:39:13 +0900

    모달 디자인

--- carrot/src/components/product-detail/Modal.jsx
+++ carrot/src/components/product-detail/Modal.jsx
@@ -7,13 +7,16 @@ export default function Modal({ dispatch }) {
     <>
       <div className="modal-bg"></div>
       <div className="modal-box">
-        <div className="flex-row">
-          <span>QR 코드 스캔</span>
-          <span className="ml-auto" onClick={dispatch}>X</span>
+        <div className="p-1 flex-row">
+          <span className="p-1">QR 코드 스캔</span>
+          <span className="p-1 ml-auto" onClick={dispatch}>X</span>
         </div>
-        <div>
-          <div className="subtitle margin-tb">당근 앱으로 열기</div>
+        <hr />
+        <div className="modal-content ">
+          <div className="subtitle ">당근 앱으로 열기</div>
           <img src={qr} alt="" />
+          <p>모바일에서만 이용할 수 있어요.</p>
+          <p>휴대전화의 카메라 또는 당근 앱으로 QR코드를 촬영해주세요.</p>
         </div>
 
       </div>
